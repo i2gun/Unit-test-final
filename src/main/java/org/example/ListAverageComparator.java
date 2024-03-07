cf82ecf4bd19c02c0368ee8ad27d5ee9e0b92f2e
i2gun <i2gun@bk.ru>
2024-03-07 17:44:13 +0500

    Adding ListAverageComparator class to the project

---
+++ src/main/java/org/example/ListAverageComparator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class ListAverageComparator {
+    List<Number> list1;
+    List<Number> list2;
+
+    ListAverageComparator(List<Number> firstlist, List<Number> secondlist) {
+        this.list1 = firstlist;
+        this.list2 = secondlist;
+    }
+}
